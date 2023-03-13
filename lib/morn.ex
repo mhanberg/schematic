2d490d69619590a86ed849e3e551a3a7f0736c3f
Mitchell Hanberg <mitch@mitchellhanberg.com>
2023-03-12 20:31:52 -0400

    the basics

--- lib/morn.ex
+++ lib/morn.ex
@@ -1,18 +1,159 @@
 defmodule Morn do
-  @moduledoc """
-  Documentation for `Morn`.
-  """
+  defmodule Schematic do
+    defstruct [:permeate, :absorb, :kind]
+  end
 
-  @doc """
-  Hello world.
+  def str(literal \\ nil) do
+    %Schematic{
+      kind: "string",
+      permeate: fn input ->
+        # FIXME this is ugly
+        cond do
+          is_binary(literal) ->
+            if is_binary(input) && input == literal do
+              {:ok, fn -> input end}
+            else
+              {:error, equality_error_str(input, literal)}
+            end
 
-  ## Examples
+          is_binary(input) ->
+            {:ok, fn -> input end}
 
-      iex> Morn.hello()
-      :world
+          true ->
+            {:error, "#{inspect(input, pretty: true)} is not a string"}
+        end
+      end
+    }
+  end
 
-  """
-  def hello do
-    :world
+  def int(literal \\ nil) do
+    %Schematic{
+      kind: "integer",
+      permeate: fn input ->
+        # FIXME this is ugly
+        cond do
+          is_integer(literal) ->
+            if is_integer(input) && input == literal do
+              {:ok, fn -> input end}
+            else
+              {:error, equality_error_str(input, literal)}
+            end
+
+          is_integer(input) ->
+            {:ok, fn -> input end}
+
+          true ->
+            {:error, "#{inspect(input, pretty: true)} is not an int"}
+        end
+      end
+    }
+  end
+
+  def list() do
+    %Schematic{
+      kind: "list",
+      permeate: fn input ->
+        if is_list(input) do
+          {:ok, fn -> input end}
+        else
+          {:error, ~s|#{inspect(input, pretty: true)} is not a list|}
+        end
+      end
+    }
+  end
+
+  def list(schematic) do
+    %Schematic{
+      kind: "list",
+      permeate: fn input ->
+        if is_list(input) do
+          Enum.reduce_while(input, {:ok, fn -> [] end}, fn el, {:ok, absorber_acc} ->
+            case permeate(schematic, el) do
+              {:ok, absorber} ->
+                absorber = fn ->
+                  absorbed_el = absorber.()
+                  absorbed_rest = absorber_acc.()
+
+                  [absorbed_el | absorbed_rest]
+                end
+
+                {:cont, {:ok, absorber}}
+
+              {:error, error} ->
+                {:halt, {:error, ~s|#{error} in #{inspect(input, pretty: true)}|}}
+            end
+          end)
+          |> then(fn
+            {:ok, absorber} ->
+              {:ok, fn -> Enum.reverse(absorber.()) end}
+
+            error ->
+              error
+          end)
+        else
+          {:error, ~s|#{inspect(input, pretty: true)} is not a list|}
+        end
+      end
+    }
+  end
+
+  def map(blueprint \\ %{}) do
+    %Schematic{
+      kind: "map",
+      permeate: fn input ->
+        if is_map(input) do
+          bp_keys = Map.keys(blueprint)
+
+          Enum.reduce_while(bp_keys, {:ok, fn -> input end}, fn bpk, {:ok, absorber_acc} ->
+            schematic = blueprint[bpk]
+
+            if schematic do
+              case permeate(schematic, input[bpk]) do
+                {:ok, absorber} ->
+                  absorber = fn ->
+                    Map.put(absorber_acc.(), bpk, absorber.())
+                  end
+
+                  {:cont, {:ok, absorber}}
+
+                {:error, error} ->
+                  {:halt, {:error, "#{error} for key #{inspect(bpk, pretty: true)} in #{inspect(input, pretty: true)}"}}
+              end
+            else
+              {:halt, {:error, "#{inspect(input, pretty: true)} is missing a #{inspect(bpk, pretty: true)} key"}}
+            end
+          end)
+        else
+          {:error, "#{inspect(input, pretty: true)} is not a map"}
+        end
+      end
+    }
+  end
+
+  def oneof(schematics) do
+    %Schematic{
+      kind: "oneof",
+      permeate: fn input ->
+        inquiry =
+          Enum.find_value(schematics, fn schematic ->
+            with {:error, _} <- permeate(schematic, input) do
+              false
+            end
+          end)
+
+        with nil <- inquiry do
+          {:error,
+           ~s|#{inspect(input, pretty: true)} is not one of: [#{Enum.map_join(schematics, ", ", & &1.kind)}]|}
+        end
+      end
+    }
+  end
+
+  def permeate(schematic, input) do
+    schematic.permeate.(input)
+  end
+
+  defp equality_error_str(input, literal) do
+    "#{inspect(input, pretty: true)} != #{inspect(literal, pretty: true)}"
   end
 end
--- test/morn_test.exs
+++ test/morn_test.exs
@@ -1,8 +1,222 @@
 defmodule MornTest do
   use ExUnit.Case
-  doctest Morn
 
-  test "greets the world" do
-    assert Morn.hello() == :world
+  import Morn
+
+  defmodule Request do
+    defstruct [:jsonrpc, :method, :params, :id]
+  end
+
+  describe "permeate" do
+    test "str/0" do
+      schematic = str()
+
+      input = "lsp is kool"
+
+      assert {:ok, absorber} = permeate(schematic, input)
+      assert input == absorber.()
+
+      assert {:error, "1 is not a string"} = permeate(schematic, 1)
+    end
+
+    test "str/1" do
+      schematic = str("lsp is kool")
+
+      input = "lsp is kool"
+
+      assert {:ok, absorber} = permeate(schematic, input)
+      assert input == absorber.()
+
+      for i <- ["lsp is lame", 1, Map.new(), Keyword.new(), {nil}] do
+        assert {:error, ~s|#{inspect(i)} != "lsp is kool"|} == permeate(schematic, i)
+      end
+    end
+
+    test "int/0" do
+      schematic = int()
+
+      input = 999
+
+      assert {:ok, absorber} = permeate(schematic, input)
+      assert input == absorber.()
+
+      assert {:error, ~s|"uh oh" is not an int|} = permeate(schematic, "uh oh")
+    end
+
+    test "int/1" do
+      schematic = int(999)
+
+      input = 999
+
+      assert {:ok, absorber} = permeate(schematic, input)
+      assert input == absorber.()
+
+      for i <- ["lsp is lame", 1, Map.new(), Keyword.new(), {nil}] do
+        assert {:error, ~s|#{inspect(i)} != 999|} == permeate(schematic, i)
+      end
+    end
+
+    test "map/0" do
+      schematic = map()
+
+      input = %{}
+
+      assert {:ok, absorber} = permeate(schematic, input)
+      assert input == absorber.()
+
+      assert {:error, ~s|"uh oh" is not a map|} = permeate(schematic, "uh oh")
+    end
+
+    test "list/0" do
+      schematic = list()
+
+      input = ["hello", "there"]
+
+      assert {:ok, absorber} = permeate(schematic, input)
+      assert input == absorber.()
+
+      input = %{}
+
+      assert {:error, ~s|%{} is not a list|} = permeate(schematic, input)
+    end
+
+    test "list/1" do
+      schematic = list(int())
+
+      input = [1, 2, 3]
+
+      assert {:ok, absorber} = permeate(schematic, input)
+      assert input == absorber.()
+
+      input = ["hi", "there"]
+
+      assert {:error, ~s|"hi" is not an int in ["hi", "there"]|} = permeate(schematic, input)
+    end
+
+    test "oneof/1" do
+      schematic = oneof([int(), str()])
+
+      input = 1
+      assert {:ok, absorber} = permeate(schematic, input)
+      assert input == absorber.()
+
+      input = "hi"
+      assert {:ok, absorber} = permeate(schematic, input)
+      assert input == absorber.()
+
+      input = []
+      assert {:error, ~s|[] is not one of: integer, string|} = permeate(schematic, input)
+    end
+
+    test "map/1" do
+      schematic =
+        map(%{
+          "foo" => str()
+        })
+
+      input = %{"foo" => "hi there!", "bar" => []}
+
+      assert {:ok, absorber} = permeate(schematic, input)
+      assert input == absorber.()
+
+      input = %{"foo" => 1, "bar" => []}
+
+      assert {:error, ~s|1 is not a string for key "foo" in %{"bar" => [], "foo" => 1}|} =
+               permeate(schematic, input)
+    end
+
+    test "complex" do
+      schematic =
+        map(%{
+          "foo" => oneof([str(), int()]),
+          "bar" =>
+            map(%{
+              "alice" => str("Alice"),
+              "bob" => list(str()),
+              "carol" =>
+                map(%{
+                  "baz" =>
+                    oneof([
+                      map(%{"one" => int()}),
+                      map(%{"two" => str()})
+                    ])
+                })
+            })
+        })
+
+      input = %{
+        "foo" => "hi there!",
+        "bar" => %{
+          "alice" => "Alice",
+          "bob" => ["is", "the", "coolest"],
+          "carol" => %{
+            "baz" => %{
+              "two" => "the second"
+            }
+          }
+        }
+      }
+
+      assert {:ok, absorber} = permeate(schematic, input)
+      assert input == absorber.()
+
+      input = %{
+        "foo" => "hi there!",
+        "bar" => %{
+          "alice" => "Alice",
+          "bob" => ["is", "the", "coolest"],
+          "carol" => %{
+            "baz" => %{
+              "three" => "the third"
+            }
+          }
+        }
+      }
+
+      expected_error = ~s"""
+      %{"three" => "the third"} is not one of: [map, map] for key "baz" in %{"baz" => %{"three" => "the third"}} for key "carol" in %{
+        "alice" => "Alice",
+        "bob" => ["is", "the", "coolest"],
+        "carol" => %{"baz" => %{"three" => "the third"}}
+      } for key "bar" in %{
+        "bar" => %{
+          "alice" => "Alice",
+          "bob" => ["is", "the", "coolest"],
+          "carol" => %{"baz" => %{"three" => "the third"}}
+        },
+        "foo" => "hi there!"
+      }
+      """ |> String.trim()
+
+      assert {:error, actual_error} = permeate(schematic, input)
+
+      assert actual_error == expected_error
+    end
+
+    # test "works" do
+    #   schematic =
+    #     map(Request, %{
+    #       {"jsonrpc", :jsonrpc} => str("2.0"),
+    #       {"method", :method} => str("initialize"),
+    #       {"params", :params} => map(%{}),
+    #       {"id", :id} => int()
+    #     })
+
+    #   input = %{
+    #     "jsonrpc" => "2.0",
+    #     "method" => "initialize",
+    #     "params" => %{},
+    #     "id" => 1
+    #   }
+
+    #   assert {:ok, absorber} = permeate(schematic, input)
+
+    #   assert %Request{
+    #            jsonrpc: "2.0",
+    #            method: "initialize",
+    #            params: %{},
+    #            id: 1
+    #          } == absorber.()
+    # end
   end
 end
